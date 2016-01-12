c640d72de9f83b120eab824455ad6c8c49a44c64
YMY <398411773@qq.com>
2016-01-12 20:32:13 +0800

    #1.4.1 修改文字

--- DSImageViewRound/Controller/RootViewController.m
+++ DSImageViewRound/Controller/RootViewController.m
@@ -40,14 +40,14 @@
                            @"ScrollView drawInRect测试(后台线程)", //UIScrollView  图片圆角后台线程绘制，内存占用超过1G很容易崩溃(第一次就点击这个内存超过1G，如果先点击上面一个再点击这个内存250M左右)
                            @"TableView drawInRect测试(主线程)",  //TableView  图片圆角主线程绘制，滚动的时候有卡顿(内存250M左右)
                            @"TableView drawInRect测试(后台线程)", //TableView  图片圆角后台线程绘制，内存占用没有超过1G的没崩溃(内存250M左右)
-                           @"在SDWebImage库里处理图片时绘制圆角"
+                           @"SDWebImage处理图片时绘制圆角测试"
                            ],
                    
                    @"ImageViewRound":@[
                            @"setCornerRadius最简单的设置圆角",
                            @"setCornerRadius设置圆角后光栅化",
                            @"图片覆盖设置圆角",
-                           @"UIimage drawInRect绘制圆角",
+                           @"UIImage drawInRect绘制圆角",
                            @"SDWebImage处理图片时绘制圆角(最好)"
                            ],
                };
